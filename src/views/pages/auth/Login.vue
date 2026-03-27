d427778cd7968d18bfbdd56ee1cc1cffdef1ac39
Ivano Rossi Sugiyanto <ipanrossi2@gmail.com>
2026-03-27 15:09:35 +0700

    fixs login

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -12,7 +12,7 @@ const checked = ref(false);
             <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="text-center mb-8">
-                        <!-- <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-8 w-16 shrink-0 mx-auto">
+                        <svg viewBox="0 0 54 40" fill="none" xmlns="http://www.w3.org/2000/svg" class="mb-8 w-16 shrink-0 mx-auto">
                             <path
                                 fill-rule="evenodd"
                                 clip-rule="evenodd"
@@ -28,8 +28,7 @@ const checked = ref(false);
                                     fill="var(--primary-color)"
                                 />
                             </g>
-                        </svg> -->
-                        <img src="/src/assets/logo3.png" />
+                        </svg>
                         <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Welcome to POS</div>
                         <span class="text-muted-color font-medium">Sign in to continue</span>
                     </div>
